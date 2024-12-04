1c1ea1dfd1ac39058b95f25b83bd9cd2f0402475
imoleFolio <dremkay71@gmail.com>
2024-12-04 15:58:42 +0100

    signup done

--- backend/utils/helpers.go
+++ backend/utils/helpers.go
@@ -1,18 +1,5 @@
 package utils
 
-import "errors"
-
-func ValidateSignupData(data *SignupData) (err error) {
-	if data.Email == "" {
-		err = errors.New("email is required")
-		return
-	} else if data.Password == "" {
-		err = errors.New("password is required")
-		return
-	}
-	return nil
-}
-
 type SignupData struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
