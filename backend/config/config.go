56db1baf42d95e87ca3542af43b0442dee7f7029
imoleFolio <dremkay71@gmail.com>
2024-12-04 11:15:12 +0100

    environment variable used,gin pakage installed and initialized

---
+++ backend/config/config.go
@@ -0,0 +1,23 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadEnv() (err error) {
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return
+}
+
+var _ = LoadEnv()
+
+var host string = os.Getenv("SERVER_HOST")
+var port string = os.Getenv("SERVER_PORT")
+var Server_address = fmt.Sprintf("%s:%s", host, port)
--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"hms/config"
+	"hms/routes"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	r := gin.Default()
+
+	r.GET("/", routes.Index)
+	r.GET("/health", routes.Health)
+
+	if err := r.Run(config.Server_address); err != nil {
+		log.Fatal(err)
+	}
 }
