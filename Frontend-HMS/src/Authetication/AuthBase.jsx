05bf01b1968fcabf1466e9d030c682cc80f4d0fc
Esseh12 <gbenmoese788@gmail.com>
2024-12-06 11:51:53 +0100

    add base authentication component for shared layout

---
+++ Frontend-HMS/src/Authetication/AuthBase.jsx
@@ -0,0 +1,12 @@
+// most of the pages look alike so this is the base component for all the pages
+
+const AuthBase = ({ children }) => {
+  return (
+    <>
+      <div>
+        <h1>Hello parent</h1>
+      </div>
+    </>
+  );
+};
+export default AuthBase;
