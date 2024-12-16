cebb69b3728a0cc13b93dc14b3df4da4e9ffd1fe
imoleFolio <dremkay71@gmail.com>
2024-12-16 12:01:05 +0100

    'X-Content-Type-Options=nosniff' header added to the cors middleware to correct corb

--- backend/middleware/cors.go
+++ backend/middleware/cors.go
@@ -11,6 +11,9 @@ func CORSMiddleware() gin.HandlerFunc {
 		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
 
+		// hopefully this helps for CORB (Cross Origin Read Block)
+		c.Header("X-Content-Type-Options", "nosniff")
+
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
 			return
